2ab3e9eaf8b14c898a503caa42899316e8ee2263
louzanizineddine <louzanizineddine@gmail.com>
2022-07-16 21:29:25 +0100

    created basic compenents

---
+++ my-app/src/routes/movie/[id].svelte
@@ -0,0 +1,12 @@
+<script context="module">
+    export async function load({fetch}) {
+        const res = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=27c689dd8b32b02b2db271915575bf32&language=en-US&page=1")
+        const data = await res.json();
+        console.log(data)
+        if (res.ok) {
+            return {
+                props: { popular: data.results }
+            }
+        }
+    }
+</script>
